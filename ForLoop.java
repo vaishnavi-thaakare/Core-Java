e6e05f6ee78ea2399b300ea7d4d3508a1f1f47ee
vaishnavi-thaakare <93474040+vaishnavi-thaakare@users.noreply.github.com>
2022-11-01 21:34:28 +0530

    Loop- for loop

---
+++ ForLoop.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class ForLoop {
+	public static void main(String[] args) {
+		// For loop - Star Pattern
+		for (int i=0;i<=4;i++) {
+			for (int j=0;j<=i;j++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+	}
+	
+
+}
