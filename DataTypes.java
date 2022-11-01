51c3314acc8990fa69cb143e1b4aded0adb79777
vaishnavi-thaakare <93474040+vaishnavi-thaakare@users.noreply.github.com>
2022-11-01 21:20:42 +0530

    Data types in java-primitive data type

---
+++ DataTypes.java
@@ -0,0 +1,28 @@
+package javaBasics;
+
+public class DataTypes {
+	/* Two data types- 1] Primitive -int,float,char,boolean,short,long,byte
+	                2] Non-primitive   */
+	public static void main(String[] args) {
+		
+		int a=2;
+		System.out.println(a +" is of int data type.");
+		float b=0.5f;
+		System.out.println(b+" is of float data type.");
+		boolean e=true;
+		System.out.println(e+" is of bollean data type.");
+		char f='a';
+		System.out.println(f +" is of char data type.");
+		byte c=125;
+		System.out.println(c+" is of byte data type");
+		double d=12456789.45879d;
+		System.out.println(d+"is of double data type.");
+	    long s=45566;
+	    System.out.println(s+" is of log data type");
+	    short q=4589;
+	    System.out.println(q+"is of short data type.");
+	    
+		
+	}
+
+}
