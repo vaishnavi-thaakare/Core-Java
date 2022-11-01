68b6ac9bd8e163cced0500c7c75aaaeca7907f41
vaishnavi-thaakare <93474040+vaishnavi-thaakare@users.noreply.github.com>
2022-11-01 21:57:13 +0530

    While loop for printing Odd Numbers

---
+++ WhileLoopForOddNumber.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class WhileLoopForOddNumber {
+	public static void main(String[] args) {
+		int i=0;
+		System.out.println(" Priniting all Even number between from 0 to 100");
+		while (i<=100) {
+		
+			if (i%2==0) {
+				System.out.println(i);
+			}
+			i++;
+		}
+		
+	}
+
+}
