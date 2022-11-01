ea320781e1e20d33541ac4ccbba677533084bd0d
vaishnavi-thaakare <93474040+vaishnavi-thaakare@users.noreply.github.com>
2022-11-01 21:33:16 +0530

    Single Inheritance Example

---
+++ SingleInheritanceChild.java
@@ -0,0 +1,8 @@
+package com.inheritance;
+
+public class SingleInheritanceChild extends SingleInheritanceParent{
+	public static void main(String[] args) {
+		SingleInheritanceChild obj=new SingleInheritanceChild();
+		obj.method1();
+	}
+}
---
+++ SingleInheritanceParent.java
@@ -0,0 +1,10 @@
+package com.inheritance;
+
+public class SingleInheritanceParent {
+	public void method1() {
+		System.out.println(" This is parent method");
+	}
+	
+	
+
+}
