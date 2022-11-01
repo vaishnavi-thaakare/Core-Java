21593c50375503342a8c6c683655b695be5494b6
vaishnavi-thaakare <93474040+vaishnavi-thaakare@users.noreply.github.com>
2022-11-01 21:23:02 +0530

    Conditional Statement- if else statement

---
+++ IfElseStatement.java
@@ -0,0 +1,13 @@
+package ConditionalStatement;
+
+public class IfElseStatement {
+	public static void main (String[] args) {
+		int num=5;
+		if(num%2==0) {
+			System.out.println("The given number  "+ num + " is even");
+			}
+		else {
+			System.out.println("The given number is "+num+" is odd");
+		}
+		}
+}
