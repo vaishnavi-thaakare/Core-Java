b9c78d51f9f32ffcf228bbced01e069484600648
vaishnavi-thaakare <93474040+vaishnavi-thaakare@users.noreply.github.com>
2022-11-01 21:25:16 +0530

    Conditional Statement- Switch Case Statement

---
+++ SwitchcaseStatement.java
@@ -0,0 +1,22 @@
+package ConditionalStatement;
+
+public class SwitchcaseStatement {
+	public static void main(String[] args) {
+		 int a=7;
+		 switch (a) {
+		 case 9:
+			 System.out.println(" This is  John's house ");
+			 break;
+		  case 8:
+			 System.out.println("This is ankit's house");
+			 break;
+		 case 7:
+			 System.out.println(" This is reema's house ");
+			 break;
+		 case 5:
+			 System.out.println(" This is payal's house ");
+			 break;
+			 default:
+				 System.out.println(" Invalid Data");
+}}}
+    
