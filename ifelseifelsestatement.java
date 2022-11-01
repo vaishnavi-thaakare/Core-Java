14a3ebd6630591c085ff87a713e6be619408a5ee
vaishnavi-thaakare <93474040+vaishnavi-thaakare@users.noreply.github.com>
2022-11-01 21:24:16 +0530

    Conditional Statement---if  elseif  else statement

---
+++ ifelseifelsestatement.java
@@ -0,0 +1,18 @@
+package ConditionalStatement;
+
+public class ifelseifelsestatement {
+     public static void main(String[] args) {
+    	 int number=5;
+    	 if (number==1) {
+    		 System.out.println(" The given number "+number +" is associate with white color");		 
+    	 }
+    	 else if(number==2) {
+    		 System.out.println("The given number "+number +" is associate with pink color");
+    	 }
+    	 else if(number==3) {
+    		 System.out.println("The given number is "+number+" is associate with blue color");
+    	 }else {
+    		 System.out.println("The number is not associate with any color");
+    	 }
+     }
+}
