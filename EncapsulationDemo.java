973133ce6d87af837c4d19a88eb737ff7f325afc
Vaishnavi Thakare <93474040+vaishnavi-thaakare@users.noreply.github.com>
2022-11-11 19:54:23 +0530

    Encapsulation

---
+++ EncapsulationDemo.java
@@ -0,0 +1,18 @@
+package com.encapsulation;
+
+public class EncapsulationDemo {
+	public String FirstName;
+	
+public String getFirstName() {
+		return FirstName;
+	}
+
+	public void setFirstName(String firstName) {
+		FirstName = firstName;
+	}
+
+public static void method(String[] args) {
+
+}
+
+}
